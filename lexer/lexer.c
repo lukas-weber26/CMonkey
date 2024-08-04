b6a07b37d3e0d1905dc5aff12dafae87389e00d0
Lukas <lukas.weber26@gmail.com>
2024-08-04 00:30:55 +0000

    Initial lexer complete

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,5 +1,6 @@
 #include "./tokens.c" 
 #include <assert.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,12 +13,15 @@ typedef struct lexer {
 } lexer ;
 
 token * lexer_next_token(lexer * lexer);
+char * lexer_read_identifier(lexer * lexer);
+char * lexer_read_number(lexer * lexer);
 void lexer_read_char(lexer * lexer);
 token * lexer_next_token(lexer * lexer);
 lexer * lexer_create(char * input);
 token * lexer_next_token(lexer * lexer);
 void free_token(token * token);
 void free_lexer(lexer * lexer);
+void lexer_skip_whitespace(lexer * lexer);
 
 //start of testing 
 typedef struct test_next_token_struct {
@@ -26,24 +30,71 @@ typedef struct test_next_token_struct {
 } test_next_token_struct;
 
 void lexer_test_next_token() {
-	char * input = "=+(){},;";
-	#define test_length 8
-	test_next_token_struct tests [test_length+1] = {
+	char * input = "let five = 5;\n"
+	"let ten = 10;\n"
+	" \n"
+	"let add = fn(x,y) {\n"
+	"	x + y;\n"
+	"};\n"
+	"let result = add(five, ten);\n"
+	"\n"
+	"!-/*5;\n"
+	"5 < 10 > 5; \n";
+
+	//DANGER: THERE IS A CORE DIFFERENCE BETWEEN THIS AND THE BOOK:
+	//In short, c strings do not contain EOF characters, hence no EOF character is detected.
+	//EOF functionality can therefore not be tested effectively. Use an exernal file and maybe architect this 
+	//so that the EOF token is not needed.
+
+	#define test_length 36
+	test_next_token_struct tests [test_length] = {
+		{LET,"let"},
+		{IDENT,"five"},
 		{ASSIGN,"="},
-		{PLUS,"+"},
+		{INT,"5"},
+		{SEMICOLON,";"},
+		{LET,"let"},
+		{IDENT,"ten"},
+		{ASSIGN,"="},
+		{INT,"10"},
+		{SEMICOLON,";"},
+		{LET,"let"},
+		{IDENT,"add"},
+		{ASSIGN,"="},
+		{FUNCTION,"fn"},
 		{LPAREN,"("},
+		{IDENT,"x"},
+		{COMMA,","},
+		{IDENT,"y"},
 		{RPAREN,")"},
 		{LBRACE,"{"},
-		{RBRACE,"}"},
-		{COMMA,","},
+		{IDENT,"x"},
+		{PLUS,"+"},
+		{IDENT,"y"},
 		{SEMICOLON,";"},
-		{ENDFILE,"EOF"},
+		{RBRACE,"}"},
+		{SEMICOLON,";"},
+		{LET,"let"},
+		{IDENT,"result"},
+		{ASSIGN,"="},
+		{IDENT,"add"},
+		{LPAREN,"("},
+		{IDENT,"five"},
+		{COMMA,","},
+		{IDENT,"ten"},
+		{RPAREN,")"},
+		{SEMICOLON,";"},
+
+		{SEMICOLON,";"},
+		{SEMICOLON,";"},
+		{SEMICOLON,";"},
+		{SEMICOLON,";"},
+		{SEMICOLON,";"},
 	};
 
-	assert(strlen(input) == test_length);	
 	lexer * test_lexer = lexer_create(input); //clean this up
 
-	for (int i = 0; i < test_length+1; i++) {
+	for (int i = 0; i < test_length; i++) {
 		token * test_token = lexer_next_token(test_lexer); 
 		assert(tests[i].token_type == test_token->token_type);
 		assert(strcmp(tests[i].expected_literal, test_token->token_string) == 0);
@@ -78,10 +129,56 @@ lexer * lexer_create(char * input) {
 	return new_lexer;
 }
 
+char * lexer_read_identifier(lexer * lexer) {
+	int initial_position = lexer->position;
+	while (is_letter(lexer->current_char)) {
+		lexer_read_char(lexer);
+	}
+	char * identifier = malloc(sizeof(char) * (1 +lexer->position- initial_position));
+	strncpy(identifier, lexer->input + initial_position ,lexer->position - initial_position);
+	identifier[lexer->position-initial_position] = '\0';
+	assert(strlen(identifier));
+	return identifier;
+}
+
+char * lexer_read_number(lexer * lexer) {
+	int initial_position = lexer->position;
+	while (is_digit(lexer->current_char)) {
+		lexer_read_char(lexer);
+	}
+	char * identifier = malloc(sizeof(char) * (1 +lexer->position- initial_position));
+	strncpy(identifier, lexer->input + initial_position ,lexer->position - initial_position);
+	identifier[lexer->position-initial_position] = '\0';
+	assert(strlen(identifier));
+	return identifier;
+}
+
+void lexer_skip_whitespace(lexer * lexer) {
+	while (lexer->current_char == ' ' || lexer->current_char == '\t' || lexer->current_char == '\n' || lexer->current_char == '\r') {
+		lexer_read_char(lexer);
+	}
+}
+
 token * lexer_next_token(lexer * lexer) {
 	token * new_token = malloc(sizeof(token));
+	lexer_skip_whitespace(lexer); //unclear if this should go before or after the next line 
 	new_token->token_type = token_string_to_token(lexer->current_char);
-	new_token->token_string = strdup(token_token_to_string(new_token->token_type));
+
+	if (new_token->token_type == LITTERAL) { 
+		new_token->token_string = lexer_read_identifier(lexer); 
+		new_token->token_type = token_string_to_keyword(new_token->token_string); 
+		return new_token;
+	} else if (new_token->token_type == INT) {
+		new_token->token_string = lexer_read_number(lexer); 
+		return new_token;
+	} else if (new_token -> token_type == ILLEGAL) {
+		new_token->token_string = malloc(sizeof(char) * 2);
+		new_token->token_string[0] = lexer->current_char;
+		new_token->token_string[1] = '\0';
+	} else {
+		new_token->token_string = strdup(token_token_to_string(new_token->token_type));
+	}
+
 	lexer_read_char(lexer); 
 	return new_token;
 }
--- lexer/tokens.c
+++ lexer/tokens.c
@@ -5,13 +5,52 @@
 
 char * token_token_to_string(int token);
 int token_string_to_token(char current_char);
+int is_letter(char c);
+int is_digit(char c);
+char * token_keyword_to_string(int keyword); 
+int token_string_to_keyword(char * keyword); 
 
-enum token_types {ILLEGAL, ENDFILE, IDENT, INT, ASSIGN, PLUS, COMMA, SEMICOLON, LPAREN, RPAREN, LBRACE, RBRACE, FUNCTION, LET};
+enum token_types {ILLEGAL, ENDFILE, IDENT, INT, ASSIGN, PLUS, COMMA, SEMICOLON, LPAREN, RPAREN, LBRACE, RBRACE, FUNCTION, LET, LITTERAL, MINUS, BANG, ASTERISK, SLASH, LT, GT};
 
 typedef struct token {
 	int token_type; 
 	char * token_string;
 } token;
+
+int is_letter(char c) {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
+}
+
+int is_digit(char c) {
+	return (c >= '0' && c <= '9');
+}
+
+int token_string_to_keyword(char * keyword) {
+	if (strcmp(keyword, "fn") == 0) {
+		return FUNCTION;
+	}	
+	
+	if (strcmp(keyword, "let") == 0) {
+		return LET;
+	}	
+
+	return IDENT;	
+}
+
+char * token_keyword_to_string(int keyword) {
+	switch (keyword) {
+		case LET:
+			return "LET";
+			break;
+		case FUNCTION:
+			return "fn";
+			break;
+		default:
+			printf("Illegal keyword.\n");
+			exit(0);
+			break;
+	}
+}
 
 int token_string_to_token(char current_char) {
 	switch (current_char) {
@@ -39,12 +78,36 @@ int token_string_to_token(char current_char) {
 		case '}':	
 			return RBRACE;
 			break;
+		case '<':	
+			return LT;
+			break;
+		case '>':	
+			return GT;
+			break;
+		case '!':	
+			return BANG;
+			break;
+		case '*':	
+			return ASTERISK;
+			break;
+		case '-':	
+			return MINUS;
+			break;
+		case '/':	
+			return SLASH;
+			break;
 		case '\0':	
 			return ENDFILE;
 			break;
 		default:
-			printf("Invalid character in tokenizer.");
-			exit(0);
+			if (is_letter(current_char)) {
+				return LITTERAL;
+			} else if (is_digit(current_char)) {
+				return INT;
+			} else {
+				return ILLEGAL;
+			}
+			break;
 	}
 }
 
@@ -92,9 +155,31 @@ char * token_token_to_string(int token) {
 		case LET:
 			return "LET";
 			break;
+		case LITTERAL:
+			return "LITTERAL";
+			break;
+		case LT:	
+			return "<";
+			break;
+		case GT:	
+			return ">";
+			break;
+		case BANG:
+			return "!";
+			break;
+		case ASTERISK:
+			return "*";
+			break;
+		case MINUS:
+			return "-";
+			break;
+		case SLASH:
+			return "/";
+			break;
 		default:
 			printf("INVALID TOKEN.\n");
 			exit(0);
 	}
 }
+
 
